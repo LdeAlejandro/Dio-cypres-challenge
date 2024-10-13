328d16fe159004ed86181b145abb324cacfe8756
Alejandro Amoroso <letzalet@gmail.com>
2024-10-13 14:49:16 -0300

    uploading files

---
+++ testAutomation/cypress/e2e/pages/login/elements.js
@@ -0,0 +1,9 @@
+export const ELEMENTS = {
+    emailInput: 'input[name="email"]',
+    passwordInput: 'input[name="password"]',
+    loginButton: '[data-cy="btnLogin"]', 
+    registerLinkText: 'Need an account?',
+    loginAccessLinkText: 'Login',
+    stringEmailNotFound: 'Email not found sign in first',
+ 
+ };
